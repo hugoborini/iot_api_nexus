3f200a708e4b02ea81e09ba5fbd1b641796f38f2
hugoborini <hugo.borini@hetic.net>
2021-07-22 17:37:13 +0200

    add post

--- controller/removeABooking.py
+++ controller/removeABooking.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, jsonify, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
 import sys
@@ -10,11 +10,17 @@ from models import model
 
 
 class removeABooking(Resource):
-    def get(self, idBooking):
+    def get(self):
+        try:
+            return {'status': "this route is for post"}
+        except:
+            return {'data': 'An Error Occurred during fetching Api'}
+    
+    def post(self):
 
-       
+        data = request.get_json()
 
-        data = model.removeABookingModel(int(idBooking))
+        data = model.removeABookingModel(int(data["idBooking"]))
 
         try:
             return {'data': data}
